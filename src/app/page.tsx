45b66dd93019cc6e71355ddc9399c5c37bc6a333
unknown <ghaniahmadzai123>
2026-02-23 23:41:14 +0500

    The Coffee section become on top in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,9 @@ export default function Home() {
   return (
     <main>
       <HeroSection />
+      <CafeSpecials />
       <FeaturedArt />
       <PopularPlants />
-      <CafeSpecials />
       <CTASection />
     </main>
   );
